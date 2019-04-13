531265cbf28585f52d4b7d059500b5cd1a3f6a9a
Jiashu Zou <zjs@JiashudeMacBook-Pro.local>
2019-04-13 18:07:46 -0400

    commit 6th query

--- app.js
+++ app.js
@@ -217,6 +217,7 @@ var PHO_players = mongoose.model("PHO_players", playerSchema, "PHO_players");
 
 var teamsView = [CHI, ATL, DET, CLE, NY, ORL, WAS, BKN, BOS, PHI, MIL, CHA, TOR, MIA, IND, LAC, DEN, GS, POR, NO, MIN, LAL, HOU, OKC, SA, UTA, SAC, DAL, MEM, PHO];
 var standingView = [CHI_standing, ATL_standing, DET_standing, CLE_standing, NY_standing, ORL_standing, WAS_standing, BKN_standing, BOS_standing, PHI_standing, MIL_standing, CHA_standing, TOR_standing, MIA_standing, IND_standing, LAC_standing, DEN_standing, GS_standing, POR_standing, NO_standing, MIN_standing, LAL_standing, HOU_standing, OKC_standing, SA_standing, UTA_standing, SAC_standing, DAL_standing, MEM_standing, PHO_standing];
+var playerView = [CHI_players, ATL_players, DET_players, CLE_players, NY_players, ORL_players, WAS_players, BKN_players, BOS_players, PHI_players, MIL_players, CHA_players, TOR_players, MIA_players, IND_players, LAC_players, DEN_players, GS_players, POR_players, NO_players, MIN_players, LAL_players, HOU_players, OKC_players, SA_players, UTA_players, SAC_players, DAL_players, MEM_players, PHO_players];
 
 app.get("/", function(req, res) {
     res.render("home");
@@ -244,6 +245,10 @@ app.get("/mongodb/teamPerformance", function(req, res) {
 
 app.get("/mongodb/allStarRank", function(req, res) {
     res.render("allStarRank");
+});
+
+app.get("/mongodb/playerHigestPerformance", function(req, res) {
+    res.render("playerHigestPerformance");
 });
 
 // app.post("/search", function(req, res) {
@@ -287,6 +292,26 @@ app.post("/mongoDB/allStarRankShow", function(req, res) {
                 else
                     res.render("allStarRankShow", {query: query});
             });
+        }
+    }
+});
+
+app.post("/mongoDB/playerHighestPerformanceShow", function(req, res) {
+    var player = req.body.player;
+    var performance = req.body.performance;
+    for (let i = 0; i < teamsView.length; i++) {
+        var team_players = teamsView[i].collection.collectionName + "_players";
+        for (let j = 0; j < playerView.length; j++) {
+            const element = playerView[j].collection.collectionName;
+            if (playerView[j].collection.collectionName == team_players) {
+                playerView[j].find({playDispNm: player}, function(err, query) {
+                    if (err)
+                        console.log(err);
+                    else
+                        res.render("playerHighestPerformanceShow", {query: query, team: team});
+                });
+            }
+            
         }
     }
 });
--- query.md
+++ query.md
@@ -1,11 +1,12 @@
 This is query part.
-<p>Input rank, relactive team and time they get the rank.</p>
-<p>Input player name, against and result (against and result could be nothing). Output player performance.</p>
-<p>Input player, rank, specific performance, output player's contribution </p>
-<p>Input team, date, ouptput team's performance (no matches, none). </p>
-<p>Input all star's date (set in the backend), output team's highest rank and date</p>
-<p>Input player, specific performance. Output date, against and highest performance. </p>
-<p>Input East/West, Output the first position's team</p>
-<p>Input player, output the count of starter.</p>
-<p>Input team, output bench player, highest average time</p>
-<p>Output all team's average OT's points.</p>
+<p>1. Input rank, relactive team and time they get the rank.</p>
+<p>2. Input player name, against and result (against and result could be nothing). Output player performance.</p>
+<p>3. Input player, rank, specific performance, output player's contribution </p>
+<p>4. Input team, date, ouptput team's performance (no matches, none). </p>
+<p>5. Input all star's date (set in the backend), output team's highest rank and date</p>
+
+<p>6. Input player, specific performance. Output date, against and highest performance. </p>
+<p>7. Input East/West, Output the first position's team</p>
+<p>8. Input player, output the count of starter.</p>
+<p>9. Input team, output bench player, highest average time</p>
+<p>10. Output all team's average OT's points.</p>
